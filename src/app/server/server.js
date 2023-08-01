c6cb334013f33828b9833a94cfe5de6b16a5feb9
Charles Brooks <cebrooks93@gmail.com>
2023-08-01 16:43:24 -0400

    fixed server lol

--- src/app/server/server.js
+++ src/app/server/server.js
@@ -1281,6 +1281,15 @@ const NA_TEAMS = [
 
 app.set("port", 3000);
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json({
     teamData: NA_TEAMS,
